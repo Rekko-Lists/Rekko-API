1e71a18b8036f61bc17bc07241b0c1f71f55f856
Eric Mejias Gamonal <ericmejiasgamonal@gmail.com>
2026-04-02 05:01:55 +0200

    Using the error handler middleware and appling to user controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,13 +1,50 @@
 import { Request, Response } from 'express';
+
 import { User } from '../domain/entities/User';
 import { CreateUserInput } from '../domain/types/user.types';
+import {
+    BadRequestError,
+    HttpError,
+    NotFoundError
+} from '../domain/errors/http.errors';
+
 import { UserPrismaRepository } from '../infraestructure/persistence/prisma/User.prisma.repository';
 import { prisma } from './../infraestructure/database/prisma.client';
 
+import { created, ok } from '../utils/response.handler';
+import { handlePrismaError } from '../domain/errors/prisma.errors';
+
 const userRepository = new UserPrismaRepository(prisma);
 
-export function getUser(req: Request, res: Response) {
-    res.status(200).send(`USER GET ${req.params.username}`);
+export async function getUser(req: Request, res: Response) {
+    try {
+        const username =
+            typeof req.params.username === 'string'
+                ? req.params.username
+                : undefined;
+
+        if (!username) {
+            throw new BadRequestError(
+                undefined,
+                'Username not valid.'
+            );
+        }
+
+        const user =
+            await userRepository.findByUsername(username);
+
+        if (!user) {
+            throw new NotFoundError(
+                undefined,
+                'User not found.'
+            );
+        }
+
+        ok(res, 'Usuario encontrado', user);
+    } catch (error) {
+        if (error instanceof HttpError) throw error;
+        handlePrismaError(error);
+    }
 }
 
 export function getUsers(req: Request, res: Response) {
@@ -25,11 +62,10 @@ export async function postUser(req: Request, res: Response) {
     } = req.body as CreateUserInput;
 
     if (!email || !password || !username) {
-        res.status(400).json({
-            message:
-                'email, password y username son obligatorios'
-        });
-        return;
+        throw new BadRequestError(
+            undefined,
+            'Email, Password and Username are required.'
+        );
     }
 
     try {
@@ -38,6 +74,7 @@ export async function postUser(req: Request, res: Response) {
             email,
             passwordHash: password,
             username,
+            reputation: 0,
             profileImage: profileImage ?? '',
             bannerImage: bannerImage ?? '',
             backgroundImage: backgroundImage ?? '',
@@ -46,17 +83,10 @@ export async function postUser(req: Request, res: Response) {
 
         await userRepository.create(user);
 
-        res.status(201).json({
-            message: 'Usuario creado correctamente'
-        });
+        created(res, 'User created succesfully.');
     } catch (error) {
-        res.status(500).json({
-            message: 'Error al crear el usuario',
-            error:
-                error instanceof Error
-                    ? error.message
-                    : 'Unknown error'
-        });
+        if (error instanceof HttpError) throw error;
+        handlePrismaError(error);
     }
 }
 
