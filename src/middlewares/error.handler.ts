06b8131b481946411f335f4a59009bddf2bccce1
Eriquito00 <150047758+Eriquito00@users.noreply.github.com>
2026-04-03 12:35:03 +0200

    Error handler with stack error

--- src/middlewares/error.handler.ts
+++ src/middlewares/error.handler.ts
@@ -1,24 +1,30 @@
 import { Request, Response, NextFunction } from 'express';
 import { HttpError } from '../domain/errors/http.errors';
 
+const isDevelopment =
+    process.env.NODE_ENV === 'development';
+
 export const errorHandler = (
     err: Error,
     req: Request,
     res: Response,
     next: NextFunction
 ): void => {
+    const stack = isDevelopment ? err.stack : undefined;
+
     if (err instanceof HttpError) {
         res.status(err.status).json({
             error: {
                 message: err.message,
-                details: err.details
+                details: err.details,
+                stack
             }
         });
     } else {
-        console.error(err);
         res.status(500).json({
             error: {
-                message: 'Internal Server Error'
+                message: 'Internal Server Error',
+                stack
             }
         });
     }
