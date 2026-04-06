c9dac87c7026ae8e515f3306bf207606d4e86a04
Eriquito00 <150047758+Eriquito00@users.noreply.github.com>
2026-04-06 13:53:39 +0200

    align user update flow and remove invalid controller declaration

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,19 +1,10 @@
 import { Request, Response } from 'express';
 
-import { User } from '../domain/entities/User';
 import { CreateUserInput } from '../domain/types/user.types';
-import {
-    BadRequestError,
-    NotFoundError
-} from '../domain/errors/http.errors';
-
-import { UserPrismaRepository } from '../infraestructure/persistence/prisma/User.prisma.repository';
-import { prisma } from './../infraestructure/database/prisma.client';
+import { userService } from '../infraestructure/container/user.container';
 
 import { created, ok } from '../utils/response.handler';
 import { catchAsync } from '../utils/catchAsync';
-
-const userRepository = new UserPrismaRepository(prisma);
 
 export const getUser = catchAsync(
     async (req: Request, res: Response) => {
@@ -22,67 +13,59 @@ export const getUser = catchAsync(
                 ? req.params.username
                 : undefined;
 
-        if (!username) {
-            throw new BadRequestError('Username not valid.');
-        }
-
         const user =
-            await userRepository.findByUsername(username);
+            await userService.getUserByUsername(username);
 
-        if (!user) {
-            throw new NotFoundError('User not found.');
-        }
-
-        ok(res, 'Usuario encontrado', user);
+        ok(res, 'User found.', user);
     }
 );
 
-export function getUsers(req: Request, res: Response) {
-    res.status(200).send(`USER GET USER LIST`);
-}
+export const getUsers = catchAsync(
+    async (req: Request, res: Response) => {
+        const users = await userService.getUsers();
+
+        ok(res, 'Users founded.', users);
+    }
+);
 
 export const postUser = catchAsync(
     async (req: Request, res: Response) => {
-        const {
-            email,
-            password,
-            username,
-            profileImage,
-            bannerImage,
-            backgroundImage
-        } = req.body as CreateUserInput;
-
-        if (!email || !password || !username) {
-            throw new BadRequestError(
-                'Email, Password and Username are required.'
-            );
-        }
-
-        const user = User.fromPersistence({
-            userId: 0,
-            email,
-            passwordHash: password,
-            username,
-            reputation: 0,
-            profileImage: profileImage ?? '',
-            bannerImage: bannerImage ?? '',
-            backgroundImage: backgroundImage ?? '',
-            createdAt: new Date()
-        });
-
-        await userRepository.create(user);
+        await userService.createUser(
+            req.body as CreateUserInput
+        );
 
         created(res, 'User created succesfully.');
     }
 );
 
-export function patchUser(req: Request, res: Response) {
-    res.status(200).send(`USER PATCH ${req.params.username}`);
-}
+export const patchUser = catchAsync(
+    async (req: Request, res: Response) => {
+        const username =
+            typeof req.params.username === 'string'
+                ? req.params.username
+                : undefined;
 
-export function deleteUser(req: Request, res: Response) {
-    res.status(200).send(`USER DELETE ${req.params.username}`);
-}
+        const 
+
+        const updated = await userService.updateUser(username);
+
+        ok(res, 'User profile updated succesfully.', updated);
+    }
+);
+
+export const deleteUser = catchAsync(
+    async (req: Request, res: Response) => {
+        const username =
+            typeof req.params.username === 'string'
+                ? req.params.username
+                : undefined;
+
+        const deleted =
+            await userService.deleteByUsername(username);
+
+        ok(res, 'User deleted succesfully.', deleted);
+    }
+);
 
 export function changeEmail(req: Request, res: Response) {
     res.status(200).send(
