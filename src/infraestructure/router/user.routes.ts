637864a7f756a0a530dafd9630906b2cf49e3e49
Eric Mejias Gamonal <ericmejiasgamonal@gmail.com>
2026-04-18 16:38:37 +0200

    Implement complete user authentication routes

--- src/infraestructure/router/user.routes.ts
+++ src/infraestructure/router/user.routes.ts
@@ -1,31 +1,64 @@
 import { Router } from 'express';
 import {
-  getUser,
-  postUser,
-  patchUser,
-  deleteUser,
-  changeEmail,
-  changePassword,
-  getUsers,
-  changeEmailConfirm
-} from '../../controllers/user.controller';
+    getUser,
+    postUser,
+    patchUser,
+    deleteUser,
+    getUsers,
+    changeUsername,
+    socialAccounts,
+    patchReputation
+} from '../../controllers/user/user.controller';
+import {
+    changeEmail,
+    changeEmailConfirm,
+    verifyEmail,
+    verifyEmailRequest
+} from '../../controllers/user/emailAuth.controller';
+import {
+    forgotPassword,
+    resetPassword
+} from '../../controllers/user/passwordAuth.controller';
+
+import {
+    validateUsername,
+    validateUserQuery
+} from '../../middlewares/validators/user.validator';
+import { parseQueryOptions } from '../../middlewares/queryOptions.middleware';
 
 const router = Router();
 
-router.route('/').get(getUsers).post(postUser);
+router
+    .route('/')
+    .get(parseQueryOptions, validateUserQuery, getUsers)
+    .post(postUser);
+
+router.use('/:username', validateUsername);
 
 router
-  .route('/:username')
-  .get(getUser)
-  .patch(patchUser)
-  .delete(deleteUser);
+    .route('/:username')
+    .get(parseQueryOptions, validateUserQuery, getUser)
+    .patch(patchUser)
+    .delete(deleteUser);
 
 router.route('/:username/change-email').post(changeEmail);
 
 router
-  .route('/:username/change-email/confirm')
-  .post(changeEmailConfirm);
+    .route('/:username/change-email/confirm')
+    .get(changeEmailConfirm);
 
-router.route('/:username/change-password').post(changePassword);
+router.route('/:username/verify-email').post(verifyEmailRequest);
+
+router.route('/:username/verify-email/confirm').get(verifyEmail);
+
+router.route('/:username/social-accounts').patch(socialAccounts);
+
+router.route('/:username/change-username').patch(changeUsername);
+
+router.route('/:username/forgot-password').post(forgotPassword);
+
+router.route('/:username/reset-password').post(resetPassword);
+
+router.route('/:username/reputation').patch(patchReputation);
 
 export default router;
