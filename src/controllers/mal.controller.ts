229d9e563d172cd08b67a27c73a17cc92efb83c9
Eric Mejias Gamonal <ericmejiasgamonal@gmail.com>
2026-05-02 14:29:20 +0200

    Mal controller

--- src/controllers/mal.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express';
-
-export function getMalAnimes(req: Request, res: Response) {
-  res.status(200).send('Animes');
-}
-
-export function getMalAnime(req: Request, res: Response) {
-  res.status(200).send(`Anime GET ${req.params.malid}`);
-}
