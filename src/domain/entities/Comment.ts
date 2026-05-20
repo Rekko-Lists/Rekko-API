7e7c845fea60c17c5cbfd9affb657fb816c1377d
Eric Mejias Gamonal <ericmejiasgamonal@gmail.com>
2026-05-20 18:15:56 +0200

    Comment prisma repo and repositories

--- src/domain/entities/Comment.ts
+++ src/domain/entities/Comment.ts
@@ -5,6 +5,9 @@ export class Comment {
     private parentCommentId: number | null;
     private message: string;
     private likes: number;
+    private user?: { username: string; profileImage: string };
+    private hasReplies?: boolean;
+    private replyCount?: number;
 
     private constructor(
         commentId: number,
@@ -12,7 +15,10 @@ export class Comment {
         postId: number,
         parentCommentId: number | null,
         message: string,
-        likes: number
+        likes: number,
+        user?: { username: string; profileImage: string },
+        hasReplies?: boolean,
+        replyCount?: number
     ) {
         this.commentId = commentId;
         this.userId = userId;
@@ -20,6 +26,9 @@ export class Comment {
         this.parentCommentId = parentCommentId;
         this.message = message;
         this.likes = likes;
+        this.user = user;
+        this.hasReplies = hasReplies;
+        this.replyCount = replyCount;
     }
 
     public static fromPersistence(data: {
@@ -29,6 +38,9 @@ export class Comment {
         parentCommentId: number | null;
         message: string;
         likes: number;
+        user?: { username: string; profileImage: string };
+        hasReplies?: boolean;
+        replyCount?: number;
     }): Comment {
         return new Comment(
             data.commentId,
@@ -36,7 +48,10 @@ export class Comment {
             data.postId,
             data.parentCommentId,
             data.message,
-            data.likes
+            data.likes,
+            data.user,
+            data.hasReplies,
+            data.replyCount
         );
     }
 
@@ -56,6 +71,28 @@ export class Comment {
         return this.parentCommentId;
     }
 
+    getMessage(): string {
+        return this.message;
+    }
+
+    getLikes(): number {
+        return this.likes;
+    }
+
+    getUser():
+        | { username: string; profileImage: string }
+        | undefined {
+        return this.user;
+    }
+
+    getReplyCount(): number | undefined {
+        return this.replyCount;
+    }
+
+    getHasReplies(): boolean | undefined {
+        return this.hasReplies;
+    }
+
     toString(): string {
         return `
             commentId=${this.commentId},
@@ -63,7 +100,10 @@ export class Comment {
             postId=${this.postId},
             parentCommentId=${this.parentCommentId},
             message=${this.message},
-            likes=${this.likes}
+            likes=${this.likes},
+            user=${this.user ? JSON.stringify(this.user) : 'null'},
+            hasReplies=${this.hasReplies},
+            replyCount=${this.replyCount}
         `;
     }
 }
